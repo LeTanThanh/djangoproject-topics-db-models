30bdd525b45d6c485bcb29c33a36d58ee07e4397
TanThanhLe <tanthanhcr95@gmail.com>
2024-07-09 00:41:14 +0700

    Add model Album

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,2 +1,3 @@
-from .person import Person
+from .album import Album
 from .musician import Musician
+from .person import Person
---
+++ app/models/album.py
@@ -0,0 +1,13 @@
+from django.db.models import (
+    CASCADE, CharField, DateField, ForeignKey, Model, PositiveIntegerField,
+)
+
+
+class Album(Model):
+    artist = ForeignKey(
+        'app.Musician',
+        on_delete=CASCADE
+    )
+    name = CharField(max_length=100)
+    release_date = DateField()
+    num_stars = PositiveIntegerField()
